0d0e2dab5a3dd99d73378badb2081ed2c8cd0262
jaruk <jarukopensource@gmail.com>
2017-02-22 19:11:33 +0100

    Java Timer/TimerTask example

--- src/main/java/com/farenda/java/util/TimerExample.java
+++ src/main/java/com/farenda/java/util/TimerExample.java
@@ -1,7 +1,30 @@
 package com.farenda.java.util;
 
-/**
- * Created by jarek on 2017-02-22.
- */
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
 public class TimerExample {
+
+    private static class WebClawlerTask extends TimerTask {
+
+        private int count = 0;
+
+        @Override
+        public void run() {
+            System.out.println("Crawling the web count: " + ++count);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Timer timer = new Timer();
+        int delay = 100;
+        int period = 200;
+        timer.scheduleAtFixedRate(new WebClawlerTask(), delay, period);
+
+        TimeUnit.SECONDS.sleep(2);
+
+        System.out.println("Stopping the timer.");
+        timer.cancel();
+    }
 }
