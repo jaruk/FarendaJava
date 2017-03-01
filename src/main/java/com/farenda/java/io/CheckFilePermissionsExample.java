5add4b453002ab8e7fd1be1ddc26b900c0b98af4
jaruk <jarukopensource@gmail.com>
2017-03-01 18:39:01 +0100

    Java IO

--- src/main/java/com/farenda/java/io/CheckFilePermissionsExample.java
+++ src/main/java/com/farenda/java/io/CheckFilePermissionsExample.java
@@ -1,7 +1,64 @@
 package com.farenda.java.io;
 
-/**
- * Created by jarek on 2017-03-01.
- */
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.DosFileAttributes;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class CheckFilePermissionsExample {
+
+    public static void main(String[] args) throws IOException {
+        List<String> names = asList("/tmp", "/root", "/bin/ls");
+
+        System.out.println("Pre Java 7 way - File.can R/W/E");
+        for (String name : names) {
+            preJava7Way(new File(name));
+        }
+
+        System.out.println("\nJava 7 Files.is R/W/E");
+        for (String name : names) {
+            usingJava7Files(name);
+        }
+
+        System.out.println("\nJava 7 using attributes from metadata");
+        for (String name : names) {
+            usingJava7Metadata(name);
+        }
+    }
+
+    private static void preJava7Way(File file) {
+        // For legacy code:
+        StringBuilder perms = new StringBuilder();
+        perms.append(file.canRead() ? 'r' : '-');
+        perms.append(file.canWrite() ? 'w' : '-');
+        perms.append(file.canExecute() ? 'x' : '-');
+        System.out.printf("Permissions of %s: %s%n",
+                file.getAbsolutePath(), perms);
+    }
+
+    private static void usingJava7Files(String name) {
+        // Simple permissions using Files from Java 7:
+        Path path = Paths.get(name);
+        StringBuilder perms = new StringBuilder();
+        perms.append(Files.isReadable(path) ? 'r' : '-');
+        perms.append(Files.isWritable(path) ? 'w' : '-');
+        perms.append(Files.isExecutable(path) ? 'x' : '-');
+        System.out.printf("Permissions of %s: %s%n",
+                path.toString(), perms);
+    }
+
+    private static void usingJava7Metadata(String name) throws IOException {
+        Path path = Paths.get(name);
+        // For DOS/Windows use DosFileAttributes.class:
+        DosFileAttributes attrs = Files.readAttributes(
+                path, DosFileAttributes.class);
+        System.out.printf("Posix permissions of %s: %s%n", name, attrs);
+    }
 }
